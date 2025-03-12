042e3adc18bbade67fd9b929ac9b47f349478621
Vigneswari Sambandam <vigneshwarie@gmail.com>
2025-03-11 18:30:20 -0700

    Instructions added

--- test.js
+++ test.js
@@ -4,3 +4,17 @@ console.log("123");
 
 
 console.log("Anoooo");
+
+
+
+
+
+//1.The workflow should trigger only when there is push in the main branch
+
+//2. Another workflow which should be triggered on demand and it should get input parameters like name, and build number and must print the input value received in the pipeline.
+
+//3. Add a secret string and print a secret string to the pipeline output
+
+//4. Set environment variable on the pipeline and print it on the pipeline job
+
+//5. Coonect to AWS and list all the available S3 buckets.
